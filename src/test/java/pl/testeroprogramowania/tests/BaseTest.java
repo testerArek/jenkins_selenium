278e70f6274c9bf122a75dfa44d548aeeec340ba
testerArek <aros25@tlen.pl>
2023-04-05 21:20:43 +0200

    add htmlReporter

--- src/test/java/pl/testeroprogramowania/tests/BaseTest.java
+++ src/test/java/pl/testeroprogramowania/tests/BaseTest.java
@@ -1,8 +1,12 @@
 package pl.testeroprogramowania.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import pl.testeroprogramowania.utils.DriverFactory;
 
 import java.util.concurrent.TimeUnit;
@@ -11,6 +15,17 @@ public class BaseTest {
 
     protected WebDriver driver;
 
+    protected static ExtentReports extentReports;
+
+    protected static ExtentHtmlReporter htmlReporter;
+
+    @BeforeSuite
+    public void beforeSuite(){
+        htmlReporter = new ExtentHtmlReporter("index.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(htmlReporter);
+    }
+
     @BeforeMethod
     public void setup() {
         driver = DriverFactory.getDriver();
@@ -18,7 +33,10 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.get("http://seleniumdemo.com/");
     }
-
+    @AfterSuite
+    public void afterSuite(){
+        extentReports.flush();
+    }
     @AfterMethod
     public void tearDown() {
         driver.quit();
--- src/test/java/pl/testeroprogramowania/tests/RegisterTest.java
+++ src/test/java/pl/testeroprogramowania/tests/RegisterTest.java
@@ -1,5 +1,7 @@
 package pl.testeroprogramowania.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,22 +13,30 @@ public class RegisterTest extends BaseTest {
     public void registerUserTest() {
         int random = (int) (Math.random() * 1000);
 
+        String username = "test" + random + "@test.pl";
+        ExtentTest extentTest = extentReports.createTest("Register user with username " + username);
+        extentTest.pass("Browser opened");
+
         WebElement dashboardLink = new HomePage(driver)
                 .openMyAccountPage()
                 .registerUserValidData("test" + random + "@test.pl", "test@test.pl")
                 .getDashboardLink();
-
+        extentTest.pass("Register steps ");
         Assert.assertEquals(dashboardLink.getText(), "Dashboard");
+        extentTest.pass("Dashboard link displayed ");
     }
 
     @Test
     public void registerUserWithSameEmailTest() {
+        ExtentTest extentTest = extentReports.createTest("Register user with username test@test.pl");
+        extentTest.pass("Browser opened");
         WebElement error = new HomePage(driver)
                 .openMyAccountPage()
                 .registerUserInvalidData("test2@test.pl", "test@test.pl")
                 .getError();
-
+        extentTest.pass("Register steps ");
         Assert.assertTrue(error.getText().contains("An account is already registered with your email address"));
+        extentTest.pass("Error message displayed ");
     }
 
 
